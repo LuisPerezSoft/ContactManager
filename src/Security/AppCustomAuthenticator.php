58412ab1031ebe2790b17f8062b18cd9429e2a5d
Luis Perez <luis.perez@assuresoft.com>
2024-09-15 19:28:25 -0400

    feat(security): add security configuration and dashboard redirection after login

--- src/Security/AppCustomAuthenticator.php
+++ src/Security/AppCustomAuthenticator.php
@@ -95,8 +95,7 @@ class AppCustomAuthenticator extends AbstractFormLoginAuthenticator implements P
             return new RedirectResponse($targetPath);
         }
 
-        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('app_dashboard'));
     }
 
     protected function getLoginUrl()
