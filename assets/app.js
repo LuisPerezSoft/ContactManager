5dc95a91ac063ee8e9ad5f46c17b3ca786b0718e
Luis Perez <luis.perez@assuresoft.com>
2024-09-15 20:05:25 -0400

    feat(ui): add styles and initial skeleton layout

--- assets/app.js
+++ assets/app.js
@@ -8,15 +8,15 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
-// import './sb-admin-2/vendor/datatables/dataTables.bootstrap4.min.css';
+import './sb-admin-2/vendor/datatables/dataTables.bootstrap4.min.css';
 
 
 // Importar Bootstrap y jQuery desde el vendor de SB Admin 2
-// import './sb-admin-2/vendor/jquery/jquery.min.js';
-// import './sb-admin-2/vendor/bootstrap/js/bootstrap.bundle.min.js';
+import './sb-admin-2/vendor/jquery/jquery.min.js';
+import './sb-admin-2/vendor/bootstrap/js/bootstrap.bundle.min.js';
 
 // Importar el JavaScript principal de SB Admin 2
-// import './sb-admin-2/js/sb-admin-2.min.js';
+import './sb-admin-2/js/sb-admin-2.min.js';
 
 
 // start the Stimulus application
